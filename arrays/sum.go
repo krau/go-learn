ae98809e1142a83ad0f1ab3ad93e2936f97ba523
krau <krau39@outlook.com>
2023-01-12 21:16:52 +0800

    重构sum

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,8 +1,9 @@
 package arrays
 
-func Sum(numbers [5]int) (sum int) {
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+func Sum(numbers [5]int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
 }
