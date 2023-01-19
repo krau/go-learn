7593cebf468ea7aeb6926b9137bb02114f69f6eb
krau <krau39@outlook.com>
2023-01-19 18:08:22 +0800

    加几句注释

--- hello/hello.go
+++ hello/hello.go
@@ -1,15 +1,18 @@
+// Hello,world
 package hello
 
 import (
 	"fmt"
 )
 
+// 定义常量，使用 const
 const englishHelloPrefix = "Hello, "
 const spanish = "Spanish"
 const chinese = "Chinese"
 const chineseHelloPrefix = "你好, "
 const spanishHelloPrefix = "Hola, "
 
+// 定义函数，使用 func 函数名字(形参1 形参1的类型,形参2 形参2的类型) 返回值的类型 { 函数代码块 }
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
@@ -26,6 +29,7 @@ func greetingPrefix(language string) (prefix string) {
 	default:
 		prefix = englishHelloPrefix
 	}
+	// 当给出了函数返回值的变量名称时，直接使用 return 返回
 	return
 }
 
--- iteration/repeat.go
+++ iteration/repeat.go
@@ -2,6 +2,7 @@ package iteration
 
 func Repeat(character string, repeatCount int) string {
 	var repeated string
+	// 迭代，使用 for
 	for i := 0; i < repeatCount; i++ {
 		repeated += character
 	}
