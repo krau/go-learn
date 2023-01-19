2c8c9c12b219281c0b1b47e21bb7e6361a3438fc
krau <krau39@outlook.com>
2023-01-19 16:29:33 +0800

    结构体，方法和接口

---
+++ structs/structs.go
@@ -0,0 +1,39 @@
+package structs
+
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+type Circle struct {
+	Radius float64
+}
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+type Rectangle struct {
+	width  float64
+	height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.height * r.width
+}
+
+func (c Circle) Area() float64 {
+	return c.Radius * c.Radius * math.Pi
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) / 2
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.width + rectangle.height)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.height * rectangle.width
+}
